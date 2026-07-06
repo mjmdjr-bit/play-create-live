8e85c6e5f167ea1553df2bb7a10547c0cb7fda46
daijiromajima-wq <daijiro.majima@toppan.co.jp>
2026-07-07 05:13:41 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -920,7 +920,7 @@ import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/cont
 
             const scene = new THREE.Scene();
             const camera = new THREE.PerspectiveCamera(38, mount.clientWidth / mount.clientHeight, 0.1, 100);
-            camera.position.set(0, 0.4, 5.4);
+            camera.position.set(0, 0.25, 3.6);
 
             const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
             renderer.setPixelRatio(Math.min(window.devicePixelRatio || 1, 2));
@@ -964,9 +964,10 @@ import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/cont
                 box.getSize(size);
                 box.getCenter(center);
                 const maxAxis = Math.max(size.x, size.y, size.z) || 1;
-                const scale = 3.2 / maxAxis;
+                const scale = 5.2 / maxAxis;
                 model.scale.setScalar(scale);
                 model.position.sub(center.multiplyScalar(scale));
+                model.position.y = 0;
             }
 
             function disposeModel(model) {
